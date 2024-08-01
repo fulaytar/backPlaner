ce08a92275c93b23346e22842823c3ae0a9249dc
fulaytar <fulaytar2405@gmail.com>
2024-08-01 22:17:37 +0300

    Update getPlanerController.js

--- src/controllers/getPlanerController.js
+++ src/controllers/getPlanerController.js
@@ -3,21 +3,6 @@ import {
   getPlannerById,
 } from '../services/timePlanerServices.js';
 
-export const getPlannerByIdControler = async (req, res) => {
-  const { id } = req.params;
-  const dataById = await getPlannerById(id);
-
-  if (!dataById) {
-    throw new Error(`Sorry not found ${id}`);
-  }
-
-  res.status(200).send({
-    status: 200,
-    message: `Successfully found ${id}`,
-    data: dataById,
-  });
-};
-
 export const getAllPlannerController = async (req, res) => {
   const allData = await getAllPlanner();
 
@@ -25,5 +10,23 @@ export const getAllPlannerController = async (req, res) => {
     status: 200,
     message: `Successfully found all planner `,
     data: allData,
+  });
+};
+
+export const getPlannerByIdControler = async (req, res) => {
+  const { id } = req.params;
+  const dataById = await getPlannerById(id);
+
+  if (!dataById) {
+    return res.status(404).send({
+      status: 404,
+      message: `Sorry, not found ${id}`,
+    });
+  }
+
+  res.status(200).send({
+    status: 200,
+    message: `Successfully found ${id}`,
+    data: dataById,
   });
 };
